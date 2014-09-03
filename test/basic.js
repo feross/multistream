90474a06e9356a5a6237b72123713b145347adb8
Feross Aboukhadijeh <feross@feross.org>
2014-09-03 00:55:00 -0700

    use string-to-stream

--- test/basic.js
+++ test/basic.js
@@ -1,36 +1,17 @@
 var concat = require('concat-stream')
-var through = require('through')
-var inherits = require('inherits')
 var MultiStream = require('../')
-var stream = require('stream')
+var str = require('string-to-stream')
 var test = require('tape')
-
-inherits(StringStream, stream.Readable)
-
-function StringStream (str) {
-  stream.Readable.call(this)
-  this._str = str
-}
-
-StringStream.prototype._read = function(n) {
-  if (!this.ended) {
-    var self = this
-    process.nextTick(function () {
-      self.push(new Buffer(self._str))
-      self.push(null)
-    })
-    this.ended = true
-  }
-}
+var through = require('through')
 
 test('combine streams', function (t) {
   var streams = [
-    new StringStream('1'),
-    new StringStream('2'),
-    new StringStream('3')
+    new str('1'),
+    new str('2'),
+    new str('3')
   ]
 
-  MultiStream(streams)
+  new MultiStream(streams)
     .on('error', function (err) {
       t.fail(err)
     })
@@ -47,7 +28,7 @@ test('combine streams (classic)', function(t) {
     through()
   ]
 
-  MultiStream(streams)
+  new MultiStream(streams)
     .on('error', function (err) {
       t.fail(err)
     })
@@ -63,16 +44,16 @@ test('combine streams (classic)', function(t) {
 
 test('lazy stream creation', function (t) {
   var streams = [
-    new StringStream('1'),
+    new str('1'),
     function() {
-      return new StringStream('2')
+      return new str('2')
     },
     function() {
-      return new StringStream('3')
+      return new str('3')
     }
   ]
 
-  MultiStream(streams)
+  new MultiStream(streams)
     .on('error', function (err) {
       t.fail(err)
     })
